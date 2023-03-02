adab8612e4efbd331c74aa1b9d8d59d8182058d5
Michael Pace <michael.g.pace@gmail.com>
2023-03-02 01:30:51 +0000

    added requires to servjs

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 'use strict';
-
-require('dotenv').config();
+console.log('server is connected.');
+const axios = require('axios');
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+const app = express();
+app.use(cors());
+
+
+
+
