91c254ab487aaf17dd16f1aaf58ae69c6c71e95d
Michael Pace <michael.g.pace@gmail.com>
2023-03-03 01:46:00 +0000

    added mongoose connect to server and dburl to envsample

--- server.js
+++ server.js
@@ -6,22 +6,28 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose')
+
 app.use(cors());
 app.use(express.json());
 const verifyUser = require('./auth');
 
-///mongoose?
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Mongoose is connected');
+});
 
 const PORT = process.env.PORT || 5005
+
 
 app.get('/', (request, response) => {
     response.send('hello from the server.');
 });
 
-
 app.use(verifyUser);
-///////////////////
-
 
 app.get("*", (request, response) => {
     response.status(404).send("Not found");
