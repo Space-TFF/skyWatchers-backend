9373ba10cec51b14bb770e297673c7e16d1a04bd
Michael Pace <michael.g.pace@gmail.com>
2023-03-03 02:12:11 +0000

    fairly generic CRUD funcs made

--- server.js
+++ server.js
@@ -29,6 +29,58 @@ app.get('/', (request, response) => {
 
 app.use(verifyUser);
 
+
+//Change ROUTE to actual Route name
+// match func names too
+
+app.get('/ROUTE', getPlace);
+app.post('ROUTE', postPlace);
+app.delete('/ROUTE/:id', deletePlace);
+app.put('/ROUTE/:id', updatePlace);
+
+async function getPlace(request, response) {
+    try {
+        let placeResults = await  Place.find({ email: request.user.email });
+        response.status(200).send(placeResults);
+    } catch (error) {
+        console.error(error);
+        response.status(500).send('server error');
+    }
+}
+
+async function postPlace(request, response, next) {
+    try {
+        let createPlace = await Place.create(request.body);
+        response.status(200).send(createPlace);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function deletePlace(request, response, next) {
+    let id = request.params.id;
+    try {
+        await Place.findByIdAndDelete(id);
+        response.status(200).send('Place deleted');
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function updatePlace(request, response, error) {
+    let id = request.params.id;
+    let placeData = request.body;
+    try {
+        let updatedPlace = await Place.findByIdAndUpdate(id, { placeData, email: request.user.email },{
+            new: true,
+            overwrite: true
+        });
+        response.status(200).send(updatedPlace);
+    } catch (error) {
+        next(error);
+    }
+}
+
 app.get("*", (request, response) => {
     response.status(404).send("Not found");
 })
